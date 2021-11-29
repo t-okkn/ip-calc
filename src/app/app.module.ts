b47dd10752de1c31cd83865426c8827b70e1dc24
t-okkn <okkn@okkn.net>
2021-11-30 02:39:25 +0900

    [feat] choiceコンポーネントの追加とそれに伴う表示修正

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AnswerComponent } from './answer/answer.component';
 import { ResultComponent } from './result/result.component';
+import { ChoiceComponent } from './choice/choice.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AnswerComponent,
-    ResultComponent
+    ResultComponent,
+    ChoiceComponent
   ],
   imports: [
     BrowserModule
